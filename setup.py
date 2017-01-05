67e53b8e3e16bb18e2f38324fc6fe0712d89bc51
Kris Fields <kris@avalaunch.com>
2017-01-04 23:42:31 -0500

    update setup version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-baker',
-    version='0.2',
+    version='0.21',
     packages=['django_baker'],
     include_package_data=True,
     license='BSD License',

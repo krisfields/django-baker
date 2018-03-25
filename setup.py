5cc3e10a3667ba0ef62fff2382fbf15c39de9227
alkelaun <8870050+alkelaun@users.noreply.github.com>
2018-03-24 19:41:51 -0700

    remove 2.7 support

--- setup.py
+++ setup.py
@@ -25,8 +25,6 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        # Replace these appropriately if you are stuck on Python 2.
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
 
         'Topic :: Internet :: WWW/HTTP',

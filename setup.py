fb14fbbf8703b563aa4517d7ab689c2743c50eb4
alkelaun <8870050+alkelaun@users.noreply.github.com>
2018-03-24 20:56:14 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-baker',
-    version='0.25',
+    version='0.26',
     packages=['django_baker'],
     include_package_data=True,
     license='BSD License',
